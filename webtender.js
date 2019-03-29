613e983f78bc46d5117d68f6c541f57d05110b68
Ben Falk <falk.ben@gmail.com>
2019-03-29 11:00:15 -0400

    add webtender

---
+++ webtender.js
@@ -0,0 +1,65 @@
+const cheerio = require("cheerio");
+const rp = require("request-promise-native");
+const fs = require("fs");
+var async = require("async");
+
+function process_results(body, id) {
+  let $ = cheerio.load(body);
+
+  let drink_elem = $("h1");
+  let drink = drink_elem.text().trim();
+
+  let ingred_section = $("ul").children();
+
+  let ingreds = [];
+  ingred_section.each(function(i, e) {
+    ingreds[i] = $(this)
+      .contents()
+      .first()
+      .next()
+      .text()
+      .trim()
+      .replace(/(\r\n|\n|\r)/gm, "");
+  });
+
+  let alcohol = $(
+    "body > table:nth-child(5) > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td > small"
+  )
+    .contents()
+    .text();
+  return { id: id, name: drink, ingredients: ingreds, alcohol: alcohol };
+}
+
+var urls = [];
+const max_drinks = 6217;
+for (let i = 1; i <= max_drinks; i++) {
+  let URL = "https://www.webtender.com/db/drink/" + i;
+  urls.push(URL);
+}
+
+var ps = [];
+async.mapLimit(urls, 10, function(url, callback) {
+  await ps.push(rp.get({ uri: url, encoding: "latin1" }));
+});
+
+// for (let i = 0; i < urls.length; i++) {
+//   ps.push(rp.get({ uri: url[i], encoding: "latin1" }));
+// }
+
+var obj = [];
+Promise.all(ps)
+  .then(results => {
+    for (let i = 0; i < results.length; i++) {
+      obj[i] = process_results(results[i], i + 1);
+    }
+  })
+  .then(() => {
+    fs.writeFile("webtender.json", JSON.stringify(obj), "utf8", err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Success");
+    });
+  })
+  .catch(err => console.log(err));
